6b6edf559f72b874524a6b1916e0bd06772dbe1a
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-03 23:43:24 +0200

    test tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // import { useState } from 'react'
+import './index.css'
 
 function App() {
-
   return (
     <>
-      <h1>Garvin Chimone</h1>
+      <h1 className="text-3xl font-bold underline text-pink-600">Garvin Chimone</h1>
     </>
   )
 }
