c3453a72df48c6047c6c897120be03950e832919
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-04 18:33:15 +0200

    create components folder with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './index.css'
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-pink-600">Garvin Chimone</h1>
+      
     </>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav>
+          <h2>GARVIN</h2>
+
+          <div>
+          </div>
+        </nav>
+    )
+}
+
+export default Navbar
