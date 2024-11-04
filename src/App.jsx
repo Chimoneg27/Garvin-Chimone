94a6da82177b09555e7ee26629492b7165a7e801
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-04 19:14:28 +0200

    install mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import { useState } from 'react'
 import './index.css'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <>
-      
+      <Navbar  />
     </>
   )
 }
