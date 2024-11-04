87d09649f78c6f831d38f45603a0806826b5901e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-04 19:14:12 +0200

    improve navbar layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,11 @@
 const Navbar = () => {
     return (
-        <nav>
-          <h2>GARVIN</h2>
+        <nav className="p-6 flex flex-row justify-between">
+          <h2 className="text-4xl font-bold w-1/5">GARVIN</h2>
 
-          <div>
+          <div className="w-40 flex flex-row justify-between">
+            <p className="w-1/2">LinkedIn</p>
+            <p className="w-1/2">Github</p>
           </div>
         </nav>
     )
