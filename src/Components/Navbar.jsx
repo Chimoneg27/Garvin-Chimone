45560b249943e24a2e39f979215dd83ca104c296
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-05 17:00:59 +0200

    add mui icons and finsih navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,14 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 const Navbar = () => {
     return (
         <nav className="p-6 flex flex-row justify-between">
           <h2 className="text-4xl font-bold w-1/5">GARVIN</h2>
 
-          <div className="w-40 flex flex-row justify-between">
-            <p className="w-1/2">LinkedIn</p>
-            <p className="w-1/2">Github</p>
+          <div className="w-20 flex flex-row justify-between text-4xl">
+            <a href="https://www.linkedin.com/in/garvin-chimone/" target="_blank"><LinkedInIcon fontSize="inherit" className="w-1/2">LinkedIn</LinkedInIcon></a>
+            <a href="https://github.com/Chimoneg27" target="_blank"><GitHubIcon fontSize="inherit" className="w-1/2">GitHub</GitHubIcon></a>
           </div>
         </nav>
     )
