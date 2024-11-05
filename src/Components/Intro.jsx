9c2fc0b7c90c7061daf09afb364a989bebf59bc2
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-05 17:35:48 +0200

    create intro component

---
+++ src/Components/Intro.jsx
@@ -0,0 +1,11 @@
+const Intro = () => {
+    return (
+        <div>
+           <h1>Garvin Chimone</h1>
+
+           <p>Hello! I am Garvin and I am a web developer. I use the following languages. You may find some of my projects below</p>
+        </div>
+    )
+}
+
+export default Intro
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,25 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from "@mui/icons-material/LinkedIn"
+import GitHubIcon from "@mui/icons-material/GitHub"
 
 const Navbar = () => {
-    return (
-        <nav className="p-6 flex flex-row justify-between">
-          <h2 className="text-4xl font-bold w-1/5">GARVIN</h2>
+  return (
+    <nav className="p-6 flex flex-row justify-between">
+      <h2 className="text-4xl font-bold w-1/5">GARVIN</h2>
 
-          <div className="w-20 flex flex-row justify-between text-4xl">
-            <a href="https://www.linkedin.com/in/garvin-chimone/" target="_blank"><LinkedInIcon fontSize="inherit" className="w-1/2">LinkedIn</LinkedInIcon></a>
-            <a href="https://github.com/Chimoneg27" target="_blank"><GitHubIcon fontSize="inherit" className="w-1/2">GitHub</GitHubIcon></a>
-          </div>
-        </nav>
-    )
+      <div className="w-20 flex flex-row justify-between text-4xl">
+        <a href="https://www.linkedin.com/in/garvin-chimone/" target="_blank">
+          <LinkedInIcon fontSize="inherit" className="w-1/2">
+            LinkedIn
+          </LinkedInIcon>
+        </a>
+        <a href="https://github.com/Chimoneg27" target="_blank">
+          <GitHubIcon fontSize="inherit" className="w-1/2">
+            GitHub
+          </GitHubIcon>
+        </a>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
