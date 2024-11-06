a5b780c836c8d44831c7c9a7c1ece7e603365c18
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-06 16:40:19 +0200

    create projects component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './index.css'
 import Navbar from './Components/Navbar'
 import Intro from './Components/Intro'
+import Projects from './Components/Projects'
 
 function App() {
   return (
     <>
       <Navbar  />
       <Intro />
+      <Projects />
     </>
   )
 }
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return (
+        <div>
+          <h2>Here are some of my projects</h2>
+        </div>
+    )
+}
+
+export default Projects
