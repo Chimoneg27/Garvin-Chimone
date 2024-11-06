38d1a2486a14bc66189fc7f0bb7894d574c46803
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-06 22:01:19 +0200

    display some of my repos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import './index.css'
 import Navbar from './Components/Navbar'
 import Intro from './Components/Intro'
 import Projects from './Components/Projects'
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/users/Chimoneg27/repos?per_page=100')
+      .then(response => {
+        setProjects(response.data.sort((a, b) => {
+          return new Date(b.updated_at) - new Date(a.updated_at)
+        }).slice(0, 5))
+      })
+  }, [])
+
   return (
-    <>
+    <div>
       <Navbar  />
       <Intro />
-      <Projects />
-    </>
+      <Projects gitArr={projects}/>
+    </div>
   )
 }
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,4 @@
-const Projects = ({gitArr}) => {
+const Projects = ({ gitArr }) => {
     return (
         <div className="w-3/4 p-3">
           <h2 className="text-3xl font-bold mt-4">Here are some of my projects</h2>
