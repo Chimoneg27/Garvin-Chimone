33502f5038d1222c7f2272ddc72bb149171b97c4
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-07 14:16:35 +0200

    add styles

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,7 +3,9 @@ const Intro = () => {
         <div className="flex flex-col items-center p-2 justify-between mt-48 h-40 mb-16">
            <h1 className="w-2/4 text-7xl text-center font-bold">GARVIN CHIMONE</h1>
 
-           <p className="w-1/2 text-center text-2xl">Hello! I am Garvin and I am a web developer. I use the following languages. You may find some of my projects below</p>
+           <p className="w-1/2 text-center text-2xl">
+              Hello! I&apos;m Garvin, a web developer dedicated to delivering top-notch solutions for your needs.
+           </p>
         </div>
     )
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,16 +1,16 @@
 const Projects = ({ gitArr }) => {
     return (
-        <div className="w-full p-3">
-          <h2 className="text-3xl font-bold mt-4">Here are some of my projects</h2>
+        <div className="w-full p-3 flex flex-col items-center">
+          <h2 className="text-4xl font-bold mt-4 mb-4">Here are some of my projects</h2>
           
-          <div className="p-3 w-full mt-5 flex flex-col items-center justify-center">
+          <div className="p-3 w-9/12 mt-5 flex flex-col items-center justify-center">
             {
               gitArr.map((project) => {
                 return (
                   <div key={project.id} className="w-4/5 h-40 mt-3 mb-3 flex flex-col items-center justify-between border border-black p-2 rounded-md">
                     <h3 className="text-2xl font-bold">{project.name}</h3>
 
-                    {project.description === null? <p className="text-xl">No description yet</p> : <p className="text-xl">{project.description}</p>}
+                    {project.description === null ? <p className="text-xl">No description yet</p> : <p className="text-xl">{project.description}</p>}
 
                     <p>Main Language: {project.language}</p>
 
