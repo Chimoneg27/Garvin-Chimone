352fe07db2c148aed04d123e593ee08a839e0281
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-11-11 18:20:34 +0200

    finish about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,22 @@ const About = () => {
           working at Growit MC
           </p>
 
-          <h2 className="text-4xl mt-6 mb-6 w-full text-center">Stats</h2>
+          <h2 className="text-4xl mt-6 mb-6 w-full text-center">Extras</h2>
+
+          <h3 className="text-3xl w-full text-center mb-1">Age</h3>
+          <p className="text-md w-full text-center mb-4">22</p>
+
+          <h3 className="text-3xl w-full text-center mb-1">Commits</h3>
+          <p className="text-md w-full text-center mb-4">2605</p>
+
+          <h3 className="text-3xl w-full text-center mb-1">Favorite Artist</h3>
+          <p className="text-md w-full text-center mb-4">Kanye West</p>
+
+          <h3 className="text-3xl w-full text-center mb-1">Currently Reading</h3>
+          <p className="text-md w-full text-center mb-4">The Bourne Trilogy</p>
+
+          <h3 className="text-3xl w-full text-center mb-1">Dream Destination</h3>
+          <p className="text-md w-full text-center mb-4">Lake Como</p>
         </div>
     )
 }
