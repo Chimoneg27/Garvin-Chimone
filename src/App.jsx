9b962160cdfaf9cceebb658c202aa8783f0564c5
Chimoneg27 <chimonegarvin27@gmail.com>
2025-06-23 18:53:11 +0200

    refactor: update About component layout and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './index.css'
+import './styles/index.css'
 import NavList from './Components/NavList'
-
+import About from './Components/About'
 function App() {
 
   return (
     <div className="w-full">
       <NavList />
+      <About />
     </div>
   )
 }
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,28 +1,14 @@
-import CountUp from "react-countup";
 const About = () => {
     return (
-        <div className="mt-10 bg-black text-white p-2 md:flex md:flex-col md:items-center">
-          <h2 className="text-5xl sm:text-center mt-4">About Me</h2>
+        <div className=" flex flex-row mt-10 text-gray-600 p-2 about w-[70%] border border-s-orange-200">
+          <div className="w-full">
+            <h1 className="text-3xl heading">Software Developer With A Focus On Web Development</h1>
 
-          <p className="mt-4 sm:text-center md:w-2/3 lg:text-xl lg:w-4/5">
-          A brief intro about me. My name is Garvin Chimone. 
-          I am web developer with a focus on the frontend. Currently 
-          working at Growit MC.
-          </p>
-
-          <h2 className="text-5xl mt-6 mb-6 w-full text-center">Extras</h2>
-
-          <h3 className="text-3xl lg:text:4xl w-full text-center mb-1">Age</h3>
-          <p className="text-md w-full lg:text-xl text-center mb-4"><CountUp start={0} end={22} duration={3} enableScrollSpy /></p>
-
-          <h3 className="text-3xl lg:text:4xl w-full text-center mb-1">Commits</h3>
-          <p className="text-md w-full lg:text-xl text-center mb-4"><CountUp start={0} end={2605} duration={5} enableScrollSpy /></p>
-
-          <h3 className="text-3xl lg:text:4xl w-full text-center mb-1">Currently Reading</h3>
-          <p className="text-md w-full lg:text-xl text-center mb-4">The Bourne Trilogy</p>
-
-          <h3 className="text-3xl lg:text:4xl w-full text-center mb-1">Dream Destination</h3>
-          <p className="text-md w-full lg:text-xl text-center mb-4">Lake Como</p>
+            <p>I&apos;m Garvin Chimone, a Full-Stack web developer passionate about learning and building
+              software that is beneficial to developers and the world at large.
+            </p>
+          </div>
+            
         </div>
     )
 }
