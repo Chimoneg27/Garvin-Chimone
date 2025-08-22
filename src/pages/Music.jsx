b26f8a52b30e625dc5310198458384d0e3de1669
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-22 14:52:44 +0200

    add footer to other pages

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function Music() {
   return (
     <div>
       <Navbar />
       <h1>Music Library</h1>
+      <Footer />
     </div>
   )
 }
