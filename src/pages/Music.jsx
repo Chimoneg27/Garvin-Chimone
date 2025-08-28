4f9ef47663f1b22a2c9ba22b9f43e134065cde78
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-28 13:08:09 +0200

    add new chart

--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useTheme } from "../components/ThemeContext";
 import { useState, useEffect } from "react";
-import { Doughnut, PolarArea, Pie } from "react-chartjs-2";
+import { Doughnut, PolarArea, Pie, Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   ArcElement,
@@ -10,8 +11,11 @@ import {
   Legend,
   RadialLinearScale,
   BarElement,
+  CategoryScale,
+  registerables
 } from "chart.js";
-ChartJS.register(ArcElement, Tooltip, Legend, RadialLinearScale, BarElement);
+ChartJS.register(ArcElement, Tooltip, Legend, RadialLinearScale, BarElement, CategoryScale, ...registerables
+);
 
 export default function Music() {
   const { color } = useTheme();
@@ -44,7 +48,6 @@ export default function Music() {
     .slice(0, 20);
 
   const labels = entriesArtists.map(([name]) => name);
-  // eslint-disable-next-line no-unused-vars
   const values = entriesArtists.map(([_, plays]) => plays);
 
   const platforms = spotifyData
@@ -148,7 +151,7 @@ export default function Music() {
     .slice(0, 20);
 
   const topSongLabels = topSongs.map(([name]) => name);
-  // eslint-disable-next-line no-unused-vars
+
   const topSongValues = topSongs.map(([_, plays]) => plays);
   const pieData = {
     labels: topSongLabels,
@@ -157,25 +160,25 @@ export default function Music() {
         label: "Plays",
         data: topSongValues,
         backgroundColor: [
-          "rgba(255, 20, 147, 0.85)",   
+          "rgba(255, 20, 147, 0.85)",
           "rgba(0, 191, 255, 0.85)",
-          "rgba(50, 205, 50, 0.85)",    
-          "rgba(255, 69, 0, 0.85)",     
-          "rgba(138, 43, 226, 0.85)",   
-          "rgba(255, 215, 0, 0.85)", 
-          "rgba(220, 20, 60, 0.85)", 
-          "rgba(0, 206, 209, 0.85)",    
-          "rgba(255, 105, 180, 0.85)",  
-          "rgba(124, 252, 0, 0.85)",    
-          "rgba(255, 140, 0, 0.85)",    
+          "rgba(50, 205, 50, 0.85)",
+          "rgba(255, 69, 0, 0.85)",
+          "rgba(138, 43, 226, 0.85)",
+          "rgba(255, 215, 0, 0.85)",
+          "rgba(220, 20, 60, 0.85)",
+          "rgba(0, 206, 209, 0.85)",
+          "rgba(255, 105, 180, 0.85)",
+          "rgba(124, 252, 0, 0.85)",
+          "rgba(255, 140, 0, 0.85)",
           "rgba(72, 61, 139, 0.85)",
-          "rgba(255, 182, 193, 0.85)",  
-          "rgba(0, 255, 127, 0.85)",    
+          "rgba(255, 182, 193, 0.85)",
+          "rgba(0, 255, 127, 0.85)",
           "rgba(219, 112, 147, 0.85)",
-          "rgba(30, 144, 255, 0.85)",   
-          "rgba(255, 99, 71, 0.85)", 
-          "rgba(186, 85, 211, 0.85)",   
-          "rgba(255, 218, 185, 0.85)",  
+          "rgba(30, 144, 255, 0.85)",
+          "rgba(255, 99, 71, 0.85)",
+          "rgba(186, 85, 211, 0.85)",
+          "rgba(255, 218, 185, 0.85)",
           "rgba(60, 179, 113, 0.85)",
         ],
         borderColor: "#fff",
@@ -185,20 +188,64 @@ export default function Music() {
   };
 
   const pieOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      display: false
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  const albums = spotifyData
+    ? spotifyData.reduce((acc, obj) => {
+        const album = obj.master_metadata_album_album_name;
+        if (album) {
+          acc[album] = (acc[album] || 0) + 1;
+        }
+        return acc;
+      }, {})
+    : 0;
+
+  const albumEntries = Object.entries(albums)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 20);
+
+  const albumLabels = albumEntries.map(([name]) => name);
+  const albumValues = albumEntries.map(([_, plays]) => plays);
+
+  const linedata = {
+    labels: albumLabels,
+    datasets: [
+      {
+        label: "Album Plays",
+        data: albumValues,
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        tension: 0.4,
+      },
+    ],
+  };
+
+  const lineOptions =  {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: true
+      }
     }
   }
-};
-
   return (
     <div className="w-full min-h-screen overflow-x-hidden">
       <Navbar />
       <div className="w-auto mt-20">
-        <h1 className="text-4xl font-bold text-center md:text-5xl" style={{ color: color }}>
+        <h1
+          className="text-4xl font-bold text-center md:text-5xl"
+          style={{ color: color }}
+        >
           Music Library
         </h1>
         <p className="text-lg font-semibold text-center p-2 sm:text-xl md:text-2xl">
@@ -210,7 +257,10 @@ export default function Music() {
         <ul className="flex flex-col gap-4 justify-center items-center sm:flex-row">
           <li className="flex flex-col justify-center items-center p-3 sm:items-center">
             {" "}
-            <h2 className="text-3xl font-semibold sm:text-2xl md:text-3xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold sm:text-2xl md:text-3xl"
+              style={{ color: color }}
+            >
               Minutes Played
             </h2>
             <p className="font-semibold text-2xl">
@@ -219,7 +269,10 @@ export default function Music() {
           </li>
           <li className="flex flex-col justify-center items-center p-3">
             {" "}
-            <h2 className="text-3xl font-semibold sm:text-2xl md:text-3xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold sm:text-2xl md:text-3xl"
+              style={{ color: color }}
+            >
               Artists Listened To
             </h2>
             <p className="font-semibold text-2xl">
@@ -228,7 +281,10 @@ export default function Music() {
           </li>
           <li className="flex flex-col justify-center items-center p-3">
             {" "}
-            <h2 className="text-3xl font-semibold sm:text-2xl md:text-3xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold sm:text-2xl md:text-3xl"
+              style={{ color: color }}
+            >
               Total Songs
             </h2>
             <p className="font-semibold text-2xl">
@@ -245,9 +301,12 @@ export default function Music() {
         >
           Charts
         </h2>
-        <ul className="list-none flex flex-col justify-center items-center w-full gap-10 mt-4 lg:flex-row">
+        <ul className="list-none flex flex-col justify-center items-center w-full gap-10 mt-4 lg:grid lg:grid-cols-3">
           <li className="flex flex-col justify-center items-center w-full">
-            <h2 className="text-3xl font-semibold md:text-4xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold md:text-4xl"
+              style={{ color: color }}
+            >
               Top 20 Artists
             </h2>
             <div className="w-[400px] h-[400px] md:w-[480px] md:h-[480px] sm:w-[450px] sm:h-[450px]">
@@ -255,7 +314,10 @@ export default function Music() {
             </div>
           </li>
           <li className="flex flex-col justify-center items-center w-full">
-            <h2 className="text-3xl font-semibold md:text-4xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold md:text-4xl"
+              style={{ color: color }}
+            >
               Platfroms Used
             </h2>
             <div className="w-[400px] h-[400px] md:w-[480px] md:h-[480px] sm:w-[450px] sm:h-[450px]">
@@ -263,14 +325,29 @@ export default function Music() {
             </div>
           </li>
           <li className="flex flex-col justify-center items-center w-full">
-            <h2 className="text-3xl font-semibold md:text-4xl" style={{ color: color }}>
+            <h2
+              className="text-3xl font-semibold md:text-4xl"
+              style={{ color: color }}
+            >
               Top 20 Songs
             </h2>
-            <div className="w-[400px] h-[400px] md:w-[480px] md:h-[480px] sm:w-[450px] sm:h-[450px]">
-              <Pie data={pieData} options={pieOptions}/>
+            <div className="w-[300px] h-[300px] md:w-[480px] md:h-[480px] sm:w-[450px] sm:h-[450px]">
+              <Pie data={pieData} options={pieOptions} />
             </div>
           </li>
         </ul>
+
+        <div className="flex flex-col justify-center items-center w-full mt-16">
+          <h2
+            className="text-3xl font-semibold md:text-4xl mb-6"
+            style={{ color: color }}
+          >
+            Top 20 Albums
+          </h2>
+          <div className="w-full max-w-[800px] h-[400px] md:h-[500px]">
+            <Line data={linedata} options={lineOptions}/>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
