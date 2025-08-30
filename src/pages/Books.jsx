16fd9543c259c3523d762058b5927b00e91de0e4
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-30 18:28:24 +0200

    update books page

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,12 +1,24 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { useTheme } from "../components/ThemeContext";
 
 export default function Books() {
+  const { color } = useTheme();
   return (
-    <div>
+    <div className="w-full min-h-screen overflow-x-hidden">
       <Navbar />
-      <h1>My Library</h1>
+      <div className="w-auto mt-20">
+        <h1
+          className="text-4xl font-bold text-center md:text-5xl"
+          style={{ color: color }}
+        >
+          Books
+        </h1>
+        <p className="text-lg font-semibold text-center p-2 sm:text-xl md:text-2xl">
+          My Book Collection
+        </p>
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
