a636845b130667d4cc7c3c2361570eeb902c3cee
Chimoneg27 <chimonegarvin27@gmail.com>
2025-09-03 21:58:04 +0200

    wrap app in auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import ThemeProvider from "../src/components/ThemeContext.jsx";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 );
