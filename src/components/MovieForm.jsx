09a28fa4e6cca9e03ac5f9408f4519b9d75b5c2a
Chimoneg27 <chimonegarvin27@gmail.com>
2025-09-08 22:14:14 +0200

    start to update movie feature for my portfolio

---
+++ src/components/MovieForm.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+export default function MovieForm() {
+  return (
+    <div>
+      <form>
+        <h2>Add a Movie or Show</h2>
+      </form>
+    </div>
+  )
+}
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,9 @@
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+// import MovieForm from "../components/MovieForm"
+// import { userRole } from "../hooks/userRoleHook"
+// import { useState, useEffect } from "react"
+// import { useTheme } from "../components/ThemeContext"
 
 export default function MoviesTV() {
   return (
