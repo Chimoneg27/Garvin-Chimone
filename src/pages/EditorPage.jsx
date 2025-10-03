52001f90dc617e7e2a021abc6b0343a56c3bed36
Chimoneg27 <chimonegarvin27@gmail.com>
2025-10-03 20:00:22 +0200

    start work on the tool bar

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,7 +1,47 @@
-import {MDXEditor, headingsPlugin} from '@mdxeditor/editor';
-import '@mdxeditor/editor/style.css';
-import { useState } from 'react';
+import {
+  MDXEditor,
+  headingsPlugin,
+  listsPlugin,
+  linkPlugin,
+  quotePlugin,
+  markdownShortcutPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  toolbarPlugin,
+  CodeToggle,
+  StrikeThroughSupSubToggles,
+  ListsToggle,
+  Separator,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+// import { useState } from "react";
 
 export default function EditorPage() {
-  return <MDXEditor markdown={'# Hello Garvin'} plugins={[headingsPlugin()]} />;
+  return (
+    <MDXEditor
+      markdown={"Hello Garvin"}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin(),
+        toolbarPlugin({
+          toolbarClassName: "tools",
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <Separator />
+              <BoldItalicUnderlineToggles />
+              <CodeToggle />
+              <Separator />
+              <ListsToggle />
+              <Separator />
+              <StrikeThroughSupSubToggles />
+            </>
+          ),
+        }),
+      ]}
+    />
+  );
 }
