61d2a11c29e55ad198dc9e6e7f04b3296265546d
Chimoneg27 <chimonegarvin27@gmail.com>
2025-10-05 23:14:09 +0200

    finish setting up the toolbar

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -12,20 +12,30 @@ import {
   StrikeThroughSupSubToggles,
   ListsToggle,
   Separator,
+  linkDialogPlugin,
+  InsertCodeBlock,
+  CreateLink,
+  imagePlugin,
+  InsertImage
 } from "@mdxeditor/editor";
 import "@mdxeditor/editor/style.css";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function EditorPage() {
+  const [body, setBody] = useState('')
+
   return (
     <MDXEditor
-      markdown={"Hello Garvin"}
+      markdown={body}
+      onChange={(newMarkdown) => setBody(newMarkdown)}
       plugins={[
         headingsPlugin(),
         listsPlugin(),
         linkPlugin(),
         quotePlugin(),
         markdownShortcutPlugin(),
+        linkDialogPlugin(),
+        imagePlugin(),
         toolbarPlugin({
           toolbarClassName: "tools",
           toolbarContents: () => (
@@ -38,6 +48,10 @@ export default function EditorPage() {
               <ListsToggle />
               <Separator />
               <StrikeThroughSupSubToggles />
+              <Separator />
+              <CreateLink />
+              <InsertCodeBlock />
+              <InsertImage />
             </>
           ),
         }),
@@ -45,3 +59,24 @@ export default function EditorPage() {
     />
   );
 }
+
+/*
+import { useState } from 'react'
+import { MDXEditor } from '@mdxeditor/editor'
+import { headingsPlugin } from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+
+function App() {
+  const [body, setBody] = useState('# Hello world')
+
+  return (
+    <MDXEditor
+      markdown={body}
+      onChange={(newMarkdown) => setBody(newMarkdown)} // update state safely
+      plugins={[headingsPlugin()]}
+    />
+  )
+}
+
+export default App
+*/
