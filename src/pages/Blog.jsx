1fccc5e36fddae448599cde49536f081fdb066de
Chimoneg27 <chimonegarvin27@gmail.com>
2025-10-12 20:40:14 +0200

    update blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,17 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-// import { useAuth } from "../auth/AuthProvider"
+import { useAuth } from "../auth/AuthProvider"
+import { userRole } from "../hooks/userRoleHook"
 import { useTheme } from "../components/ThemeContext";
+import { getMyBlogs } from "../lib/supabase";
+import { useState, useEffect } from "react";
 
 export default function Blog() {
   const { color } = useTheme();
+  const { role } = userRole()
+  const [blogs, setBlogs] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
   return (
     <div>
       <Navbar />
@@ -12,8 +19,10 @@ export default function Blog() {
         className="text-5xl md:text-6xl font-bold text-center mb-4 tracking-tight"
         style={{ color: color }}
       >
-        Blog Page Coming Soon
+        Garvin&apos;s Blog
       </h1>
+
+      <h2>A place to view my latest ideas and notes</h2>
       <Footer />
     </div>
   );
