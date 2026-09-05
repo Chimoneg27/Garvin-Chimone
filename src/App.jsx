9db2589e6a4f24dd6f3f0540c55387b00fa43d3d
Chimoneg27 <chimonegarvin27@gmail.com>
2026-09-06 00:26:09 +0200

    delete files

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
 import "./styles/index.css";
 import Home from "./pages/Home";
-import Dev from "./pages/Dev";
-import Books from "./pages/Books";
-import Music from "./pages/Music";
-import Signup from "./auth/Signup";
 import { Routes, Route } from "react-router-dom";
-import SignIn from "./auth/SignIn";
-import BooksPage from "./pages/BookPage";
-import Projects from "./pages/Projects";
-import Blog from "./pages/Blog";
-import EditorPage from "./pages/EditorPage";
-import ProtectedRoute from "./components/ProtectedRoutes";
-import BlogPost from "./pages/BlogPost";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Dev" element={<Dev />} />
-        <Route path="/MusicLibrary" element={<Music />} />
-        <Route path="/Books" element={<Books />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/Blogs" element={<Blog />} />
-        <Route path="/Blogs/:id" element={<BlogPost />} />
-        <Route path="/Books/:id" element={<BooksPage />} />
-        <Route path="/SignUp" element={<Signup />} />
-        <Route path="/SignIn" element={<SignIn />} />
-        <Route
-          path="/Blogwriter"
-          element={
-            <ProtectedRoute requiredRole="admin">
-              <EditorPage />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ThemeProvider from "../src/components/ThemeContext.jsx";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./auth/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </ThemeProvider>
-    </AuthProvider>
   </StrictMode>
 );
